85e84ab28880a25df7f4b039354ab6edea6267f1
m-yuhas <m-yuhas@qq.com>
2017-04-12 22:53:58 -0700

    Refactored Code
    Now State Machines Run in Main, with interrupts passing info via globals

--- firmware/main.c
+++ firmware/main.c
@@ -9,12 +9,32 @@
  * msp430.h - constants and pin mappings for msp430
  */
 #include <msp430.h>
+#include <stdint.h>
+
+/*
+ * Definitions:
+ */
+#define TIMER_RESOLUTION_MS		1
+#define TIMER_RESOLUTION_TICKS	125 * TIMER_RESOLUTION_MS
+#define P1_INTERRUPT_MASK		0x01
+#define P1_INPUT_MASK			0xC0
+#define P1_RESET_INTERRUPT_FLAG 0x00
+#define LEFT_SIGNAL_1_MASK		0x30
+#define LEFT_SIGNAL_2_MASK		0x28
+#define LEFT_SIGNAL_3_MASK		0x18
+#define LEFT_SIGNAL_4_MASK		0x00
+#define	LEFT_SIGNAL_ALL_MASK	0x00
+#define RIGHT_SIGNAL_1_MASK		0x03
+#define RIGHT_SIGNAL_2_MASK		0x05
+#define RIGHT_SIGNAL_3_MASK		0x06
+#define RIGHT_SIGNAL_4_MASK		0x00
+#define RIGHT_SIGNAL_ALL_MASK	0x00
 
 /*
  * Typedefs:
  * Signal - tells whether the left or right signal is active
  */
-typedef enum { LEFT_SIG, RIGHT_SIG } Signal;
+typedef enum { INIT, PATTERN_1, PATTERN_2, PATTERN_3, PATTERN_4 } State;
 
 /*
  * Globals:
@@ -23,9 +43,13 @@ typedef enum { LEFT_SIG, RIGHT_SIG } Signal;
  * next_right - the time remaining until the right signal fires
  * previous_input - the previous input from P1 to the system
  */
-Signal next_timer;
+State left_state;
+State right_state;
 unsigned int next_left;
 unsigned int next_right;
+volatile unsigned int tick_count;
+unsigned char interrupt_flag;
+volatile unsigned char input_vector;
 unsigned char previous_input;
 
 /*
@@ -38,49 +62,11 @@ unsigned char previous_input;
 #pragma vector=PORT1_VECTOR
 __interrupt void port_one_interrupt( void )
 {
-	unsigned char input_vector = P1IN & 0xC0;
-	TACTL = MC_0;
+	input_vector = P1IN & P1_INPUT_MASK;
+	interrupt_flag |= P1_INTERRUPT_MASK;
 	P1IES = input_vector;
-	P1IFG = 0x00;
-	if ( input_vector == 0x40 )
-	{
-		if ( TAR == 0 || next_timer == RIGHT_SIG )
-		{
-			CCR0 = 30000;
-			next_timer = RIGHT_SIG;
-		}
-		else
-		{
-			next_right = TAR;
-		}
-		TACTL = TASSEL_2 + MC_1 + ID_3;
-		P1OUT = ( P1OUT & 0xF8 ) | 0x03;
-		return;
-	}
-	if ( input_vector == 0x80 )
-	{
-		if ( TAR == 0 || next_timer == LEFT_SIG )
-		{
-			CCR0 = 30000;
-			next_timer = LEFT_SIG;
-		}
-		else
-		{
-			next_left = TAR;
-		}
-		TACTL = TASSEL_2 + MC_1 + ID_3;
-		P1OUT = ( P1OUT & 0xC7 ) | 0x30;
-		return;
-	}
-	if ( input_vector == 0xC0 )
-	{
-		CCR0 = 30000;
-		next_left = 10;
-		next_timer = RIGHT_SIG;
-		TACTL = TASSEL_2 + MC_1 + ID_3;
-		P1OUT = ( P1OUT & 0xC0 ) | 0x33;
-		return;
-	}
+	P1IFG = P1_RESET_INTERRUPT_FLAG;
+	__bic_SR_register_on_exit( LPM0_bits );
 	return;
 }
 
@@ -95,111 +81,107 @@ __interrupt void port_one_interrupt( void )
 __interrupt void timer_a_interrupt( void )
 {
 	TACTL = MC_0;
-	if ( next_timer == RIGHT_SIG )
-	{
-		if ( ( P1OUT & 0x07 ) == 0x03 )
-		{
-			P1OUT = ( P1OUT & 0xF8 ) | 0x05;
-			if ( next_left == 0 )
-			{
-				CCR0 = 30000;
-			}
-			else
-			{
-				CCR0 = next_left;
-				next_right = 30000 - next_left;
-				next_timer = LEFT_SIG;
-			}
-			TACTL = TASSEL_2 + MC_1 + ID_3;
-			return;
-		}
-		if ( ( P1OUT & 0x07 ) == 0x05 )
-		{
-			P1OUT = ( P1OUT & 0xF8 ) | 0x06;
-			if ( next_left == 0 )
-			{
-				CCR0 = 30000;
-			}
-			else
-			{
-				CCR0 = next_left;
-				next_right = 30000 - next_left;
-				next_timer = LEFT_SIG;
-			}
-			TACTL = TASSEL_2 + MC_1 + ID_3;
-			return;
-		}
-		if ( ( P1OUT & 0x07 ) == 0x06 )
-		{
-			P1OUT = ( P1OUT & 0xF8 ) | 0x00;
-			if ( next_left == 0 )
-			{
-				TACTL = MC_0;
-				TAR = 0;
-				return;
-			}
-			else
-			{
-				CCR0 = next_left;
-				next_timer = LEFT_SIG;
-			}
-			TACTL = TASSEL_2 + MC_1 + ID_3;
-			return;
-		}
-	}
-	else
-	{
-		if ( ( P1OUT & 0x38 ) == 0x30 )
-		{
-			P1OUT = ( P1OUT & 0xC7 ) | 0x28;
-			if ( next_right == 0 )
-			{
-				CCR0 = 30000;
-			}
-			else
-			{
-				CCR0 = next_right;
-				next_left = 30000 - next_right;
-				next_timer = RIGHT_SIG;
-			}
-			TACTL = TASSEL_2 + MC_1 + ID_3;
-			return;
-		}
-		if ( ( P1OUT & 0x38 ) == 0x28 )
-		{
-			P1OUT = ( P1OUT & 0xC7 ) | 0x18;
-			if ( next_right == 0 )
-			{
-				CCR0 = 30000;
-			}
-			else
-			{
-				CCR0 = next_right;
-				next_left = 30000 - next_right;
-				next_timer = RIGHT_SIG;
-			}
-			TACTL = TASSEL_2 + MC_1 + ID_3;
-			return;
-		}
-		if ( ( P1OUT & 0x38 ) == 0x18 )
-		{
-			P1OUT = ( P1OUT & 0xC7 ) | 0x00;
-			if ( next_right == 0 )
-			{
-				TACTL = MC_0;
-				TAR = 0;
-				return;
-			}
-			else
-			{
-				CCR0 = next_right;
-				next_timer = RIGHT_SIG;
-			}
-			TACTL = TASSEL_2 + MC_1 + ID_3;
-			return;
-		}
-	}
+	tick_count++;
+	CCR0 = TIMER_RESOLUTION_TICKS;
+	TAR = 0;
+	TACTL = TASSEL_2 + ID_3 + MC_1;
+	__bic_SR_register_on_exit( LPM0_bits );
+	return;
 }
+
+unsigned int left_state_machine( State *left_state, unsigned int *next_left, unsigned int *tick_count, unsigned int *wait_time, unsigned int *last_start )
+{
+	switch ( *left_state )
+	{
+		case INIT :
+			if ( ( input_vector & 0x80 ) == 0x80 )
+			{
+				*left_state = PATTERN_1;
+				*next_left = *tick_count + *wait_time;
+				*last_start = *tick_count;
+				P1OUT = ( P1OUT & 0x03 ) | LEFT_SIGNAL_1_MASK;
+				if ( TACTL == MC_0 )
+				{
+					TAR = 0;
+					CCR0 = TIMER_RESOLUTION_TICKS;
+					TACTL = TASSEL_2 + ID_3 + MC_1;
+				}
+			}
+			break;
+		case PATTERN_1 :
+			if ( ( input_vector & 0x80 ) != 0x80 )
+			{
+				*left_state = INIT;
+				P1OUT = ( P1OUT & 0x03 ) | LEFT_SIGNAL_ALL_MASK;
+				if ( *tick_count < *last_start )
+				{
+					return *tick_count + UINT16_MAX - *last_start;
+				}
+				return *tick_count - *last_start;
+			}
+			if ( *tick_count == *next_left )
+			{
+				*left_state = PATTERN_2;
+				*next_left = *tick_count + *wait_time;
+				P1OUT = ( P1OUT & 0x03 ) | LEFT_SIGNAL_2_MASK;
+			}
+			break;
+		case PATTERN_2 :
+			if ( ( input_vector & 0x80 ) != 0x80 )
+			{
+				*left_state = INIT;
+				P1OUT = ( P1OUT & 0x03 ) | LEFT_SIGNAL_ALL_MASK;
+				if ( *tick_count < *last_start )
+				{
+					return *tick_count + UINT16_MAX - *last_start;
+				}
+				return *tick_count - *last_start;
+			}
+			if ( *tick_count == *next_left )
+			{
+				*left_state = PATTERN_3;
+				*next_left = *tick_count + *wait_time;
+				P1OUT = ( P1OUT & 0x03 ) | LEFT_SIGNAL_3_MASK;
+			}
+			break;
+		case PATTERN_3 :
+			if ( ( input_vector & 0x80 ) != 0x80 )
+			{
+				*left_state = INIT;
+				P1OUT = ( P1OUT & 0x03 ) | LEFT_SIGNAL_ALL_MASK;
+				if ( *tick_count < *last_start )
+				{
+					return *tick_count + UINT16_MAX - *last_start;
+				}
+				return *tick_count - *last_start;
+			}
+			if ( *tick_count == *next_left )
+			{
+				*left_state = PATTERN_4;
+				*next_left = *tick_count + *wait_time;
+				P1OUT = ( P1OUT & 0x03 ) | LEFT_SIGNAL_4_MASK;
+			}
+			break;
+		case PATTERN_4 :
+			if ( ( input_vector & 0x80 ) != 0x80 )
+			{
+				*left_state = INIT;
+				P1OUT = ( P1OUT & 0x03 ) | LEFT_SIGNAL_ALL_MASK;
+				if ( *tick_count < *last_start )
+				{
+					return *tick_count + UINT16_MAX - *last_start;
+				}
+				return *tick_count - *last_start;
+			}
+			break;
+		default :
+			*left_state = INIT;
+			P1OUT = ( P1OUT & 0x03 ) | LEFT_SIGNAL_ALL_MASK;
+			break;
+	}
+	return 0;
+}
+
 
 /*
  * main - code entry point
@@ -211,19 +193,31 @@ __interrupt void timer_a_interrupt( void )
 int main( void )
 {
     WDTCTL = WDTPW | WDTHOLD;
-    next_timer = RIGHT_SIG;
     next_left = 0;
     next_right = 0;
 	P1REN = 0x00;
     P1SEL = 0x00;
 	P1DIR = 0x3F;
 	P1OUT = 0x00;
-	previous_input = P1IN & 0xC0;
 	P1IES = 0x00;
 	P1IFG = 0x00;
 	P1IE = 0xC0;
+	TACTL = MC_0;
 	CCTL0 |= CCIE;
+	unsigned int left_past[16];
+	left_state = INIT;
+	input_vector = 0x00;
+	unsigned int wait_time = 300;
+	unsigned int last_start_left = 0;
 	__enable_interrupt();
-	_BIS_SR( LPM0 + GIE );
-	return 0;
+
+	while ( 1 )
+	{
+		//if ( tick_count >= max_tick_count )
+		//{
+		//	tick_count -= max_tick_count;
+		//}
+		left_state_machine( &left_state, &next_left, &tick_count, &wait_time, &last_start_left );
+		__bis_SR_register( LPM0_bits + GIE );
+	}
 }
