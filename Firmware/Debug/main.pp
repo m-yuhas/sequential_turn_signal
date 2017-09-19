d58354f559e92605a5c98c63c1d890d5ba130ede
m-yuhas <m-yuhas@qq.com>
2017-09-18 22:19:13 -0700

    Committing Left and Right Turn Signal Code

--- Firmware/Debug/main.pp
+++ Firmware/Debug/main.pp
@@ -6,7 +6,6 @@ main.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430g2231.h
 main.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
 main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics.h
 main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics_legacy_undefs.h
-main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/stdint.h
 
 ../main.c: 
 C:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
@@ -14,4 +13,3 @@ C:/ti/ccsv6/ccs_base/msp430/include/msp430g2231.h:
 C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
 C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics.h: 
 C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics_legacy_undefs.h: 
-C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/stdint.h: 
